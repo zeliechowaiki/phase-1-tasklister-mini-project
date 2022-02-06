3b8b38e0b61d06a023f9c334b1c324fc25ef8972
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-02-06 12:02:34 -0500

    completed basic list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(e.target.new_task_description.value);
+    buildToDo(e.target.new_task_description.value);
+    form.reset();
+  })
 });
+
+function buildToDo(todo){
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  p.textContent = `${todo} `;
+  btn.textContent = 'x';
+  btn.addEventListener('click', handleDelete);
+  p.appendChild(btn);
+  document.querySelector('#tasks').appendChild(p);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
